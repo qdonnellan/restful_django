ab208654781f8d0231357f37bd7eaeb510f55783
qdonnellan <qdonnellan@gmail.com>
2014-05-01 19:21:07 -0500

    adding a handler for posting data to the api

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -1,7 +1,17 @@
 var thingAppControllers = angular.module('thingAppControllers', []);
 
-thingAppControllers.controller('ThingCtrl', ['$scope', 'Thing', 
+thingAppControllers.controller('ThingCtrl', ['$scope', 'Thing',
     function ($scope, Thing) {
         $scope.things = Thing.query();
+
+        $scope.saveThing = function() {
+            Thing.save($scope.thing, function() {
+                // a successful post!
+                $scope.things = Thing.query();
+            }, function(error) {
+                // an unsuccessful post, womp womp
+                alert('you broke it!');
+            });
+        };
     }
 ]);
